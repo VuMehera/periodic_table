973ce8a12f815d3205e6b52f8ed8e7f166a10868
VuMehera <164837734+VuMehera@users.noreply.github.com>
2026-03-09 00:56:31 +1100

    Add files via upload

--- element.sh
+++ element.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-# Check if the user provided an argument
-if [[ -z $1 ]]; then
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
   echo "Please provide an element as an argument."
-  exit 0
+  exit
 fi
 
-INPUT=$1
+ARG=$1
 
-# Detect if input is a number (atomic_number) or string (symbol/name)
-if [[ $INPUT =~ ^[0-9]+$ ]]; then
-  QUERY="SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type
-         FROM properties
-         JOIN elements USING(atomic_number)
-         JOIN types USING(type_id)
-         WHERE atomic_number=$INPUT;"
+# Check if argument is an integer (atomic number)
+if [[ $ARG =~ ^[0-9]+$ ]]
+then
+  ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+  FROM elements e 
+  JOIN properties p ON e.atomic_number=p.atomic_number 
+  JOIN types t ON p.type_id=t.type_id
+  WHERE e.atomic_number=$ARG")
 else
-  QUERY="SELECT atomic_number, name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type
-         FROM properties
-         JOIN elements USING(atomic_number)
-         JOIN types USING(type_id)
-         WHERE symbol='$INPUT' OR name='$INPUT';"
+  # Otherwise, search by symbol or name (text)
+  ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius 
+  FROM elements e 
+  JOIN properties p ON e.atomic_number=p.atomic_number 
+  JOIN types t ON p.type_id=t.type_id
+  WHERE e.symbol='$ARG' OR e.name='$ARG'")
 fi
 
-RESULT=$(psql --username=freecodecamp --dbname=periodic_table -t --no-align -c "$QUERY")
-
-if [[ -z $RESULT ]]; then
+if [[ -z $ELEMENT ]]
+then
   echo "I could not find that element in the database."
-else
-  IFS="|" read ATOMIC_NUMBER NAME SYMBOL MASS MELTING BOILING TYPE <<< "$RESULT"
-  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  exit
 fi
+
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
